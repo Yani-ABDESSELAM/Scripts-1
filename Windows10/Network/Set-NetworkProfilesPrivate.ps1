308d02ce9a88a979125c32ed3072387770009236
Cam <cameron@kollwitz.us>
2020-06-02 14:33:35 -0700

    Add scripts

--- Windows10/Network/Set-NetworkProfilesPrivate.ps1
+++ Windows10/Network/Set-NetworkProfilesPrivate.ps1
@@ -1,2 +1,3 @@
 # Set all Network Profiles to "Private"
+# This is to remediate issues with WinRM on devices.
 Get-NetConnectionProfile | Where-Object { $_.NetworkCategory -match "Public" } | Set-NetConnectionProfile -NetworkCategory Private
